3f7947f340a7750fe6acea54d86d35af6050c427
LadyBug89 <king_prawn_cocktail@hotmail.com>
2019-10-06 18:10:54 +0100

    Added immplementation of BFS

--- Graph.h
+++ Graph.h
@@ -2,19 +2,26 @@
 #define GRAPH_H
 
 #include <list>
+#include <vector>
+#include <queue>
 #include <memory>
+#include <iostream>
 
 class Graph
 {
 public:
     Graph() = default;
-    Graph(int V);
-    virtual addEdge(int u, int v) = 0;
+    Graph(int V);	
+	void BFS();
 	
-
 protected:
+    virtual addEdge(int u, int v) = 0;
+
     int V; // number of nodes in the Graph
     std::unique_ptr<std::list<int>[]> adj_list;
+	
+private:
+    void BFSUtil(int src);
 };
 
 #endif
