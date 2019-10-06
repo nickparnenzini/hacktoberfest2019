814f23565d8e4c7d92ff8f3488550f507738f1d3
testuser <testuser@example.com>
2019-10-06 19:21:36 +0100

    Added cycle detection for directed Graph

--- UndirectedGraph.cpp
+++ UndirectedGraph.cpp
@@ -1,9 +1,36 @@
 #include "UndirectedGraph.h"
 
-UndirectedGraph::UndirectedGraph(int V) {
-	
+UndirectedGraph::UndirectedGraph(int V) : Graph(V) {
 }
 
 void UndirectedGraph::addEdge(int u, int v) {
-	
+	adj_list[u].push_back(v);
+    adj_list[v].push_back(u);
+}
+
+bool UndirectedGraph::detect_cycle() {
+    std::vector<bool> visited(V, false);
+    for (int i = 0; i < V; ++i) {
+        if (!visited[i]) {
+            if (detectCycleUtil(i, visited, -1)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool UndirectedGraph::detectCycleUtil(int src, std::vector<bool>& visited, int parent) {
+    visited[src] = true;
+    for (auto it = adj_list[src].begin(); it != adj_list[src].end(); ++it) {
+        if (!visited[*it]) {
+            if (detectCycleUtil(*it, visited, src)) {
+                return true;
+            }
+        }
+        else if (*it != parent) {
+            return true;
+        }
+    }
+    return false;
 }
--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World" << std::endl;	
+    UndirectedGraph g(5);
+    g.add_edge(1, 0); 
+    g.add_edge(0, 2); 
+    g.add_edge(2, 1); 
+    g.add_edge(0, 3); 
+    g.add_edge(3, 4);
+
+    std::cout << g.detect_cycle() << std::endl;
 }
